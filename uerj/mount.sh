af3f9b544b3c1e491509f0255b465fecdfe8d7cf
Ivan Lopes <lopesivan.ufrj@gmail.com>
2023-05-19 00:17:39 -0300

    Remove o arquivo lib/showformula.hp50

--- uerj/mount.sh
+++ uerj/mount.sh
@@ -1,27 +1,21 @@
 #!/usr/bin/env bash
 
-DIRS=$(
-ls -l --time-style="long-iso" . | egrep '^d' | awk '{print $8}'
-)
-
-FILES=$(
-ls -l --time-style="long-iso" . | egrep '\.hp50$' | awk '{print $8}'
-)
-
-echo "$(pwd | sed 's=.*/==')"
+echo "\<< HOME 'main' PGDIR CLEAR"
 echo DIR
 
+cat -s ../libs/*.hp50g
 
-for f in ../lib/*.hp50; do
-  sed 's/^@.*//' "$f"
-done| cat -s | sed '/^$/d'| sed 's/^/  /'
+DIRS=$(find . -maxdepth 1 -type d| sed '1d')
+
+FILES=$(find . -maxdepth 1 -name \*.hp50)
 
 for f in "${FILES[@]}"; do
-  sed 's/^@.*//' "$f"
-done| cat -s | sed '/^$/d'| sed 's/^/  /'
+    cat -s "$f"
+done
 
-for d in "${DIRS[@]}"; do
-	(cd "$d"; ./mount.sh)
-done| cat -s | sed '/^$/d'| sed 's/^/  /'
+for d in ${DIRS[@]}; do  
+    ( cd "$d"; ./mount.sh ) 
+done
 
-echo END
+echo "END 'main' STO main conf main CLEAR"
+echo '\>>'
