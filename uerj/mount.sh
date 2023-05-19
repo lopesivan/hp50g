823e5c93c09491222ff4b0c63e7418a4c148e2bb
Ivan Lopes <lopesivan.ufrj@gmail.com>
2023-05-19 01:41:38 -0300

    Atualiza o arquivo libs/lib.hp50g

--- uerj/mount.sh
+++ uerj/mount.sh
@@ -11,6 +11,20 @@ DIRS=$(find . -maxdepth 1 -type d| sed '1d')
 
 FILES=$(find . -maxdepth 1 -name \*.hp50)
 
+cat <<EOF
+CST { 
+{ + \<< 22.2 KEYEVAL uerj \>> } 
+{ "CONF" \<< conf \>> } 
+{ HELP HELP } 
+IVAN 
+{ "" } 
+{ "" }
+}
+EOF
+
+echo uerj
+echo DIR
+
 for f in "${FILES[@]}"; do
     cat -s "$f"
 done
@@ -19,5 +33,6 @@ for d in ${DIRS[@]}; do
     ( cd "$d"; ./mount.sh ) 
 done
 
+echo END
 echo "END 'main' STO main conf main CLEAR"
 echo '\>>'
